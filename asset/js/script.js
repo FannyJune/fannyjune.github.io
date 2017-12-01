b6651685f2a0c22dff0b9ea070bd0d8291026b8b
Jihye Choe <jchoe4@id.iit.edu>
2017-11-30 22:51:53 -0600

    added radio button style

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,20 +1,3 @@
-/* function onPaymentChange(event,method) {
-    $('.switching_form').css({display:'none'});
-       switch(method){
-            case 'visa':
-                console.log('visa');
-                break;
-            case 'venmo':
-                console.log('venmo');
-                $('#venmo_form').css({display:'inline-block'});
-                break;
-            case 'paypal':
-                console.log('PAYPAL');
-                break;
-       }
-    } */
-
-
 function menuClickIndex(x) {
 
     menuClick(x);
